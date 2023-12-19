db36a93b648834a453b0c12f8efb175c1b52b49e
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-19 14:48:47 +0530

    Code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgToastModule } from 'ng-angular-popup';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+
 import { AuthService } from './services/auth.service';
 import { filter } from 'rxjs/operators';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    HttpClientModule,
-    NgToastModule,
-    HeaderComponent,
-    FooterComponent,
-  ],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, NgToastModule, HeaderComponent, FooterComponent],
   providers: [HttpClient, AuthService]
 })
 export class AppComponent implements OnInit {
-  title = 'Student_Registration_System-Frontend';
-
   isLogin: boolean = false;
   activePathName: string = "";
 
-
-  constructor(
-    private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     // Subscribe to NavigationEnd events
