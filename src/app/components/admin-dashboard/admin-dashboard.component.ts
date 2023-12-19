8a70a2dae67b7efc1bed1de83b6c2e5ca74854c4
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-19 09:30:53 +0530

    Create student registration form and validate

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -3,11 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatCardModule } from "@angular/material/card";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [HttpClientModule,MatCardModule,MatGridListModule],
+  imports: [HttpClientModule,MatCardModule,MatGridListModule,RouterModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
   providers: [HttpClient]
