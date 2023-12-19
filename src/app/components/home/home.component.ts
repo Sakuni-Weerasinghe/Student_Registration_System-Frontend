b16ebe76a414bda34d1d22a92c6e8112a1ab336e
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-19 14:40:21 +0530

    Update home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,7 +14,6 @@ export class HomeComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    const token = localStorage.getItem('token');
-    if (token) this.router.navigate(['dashboard']);
+    
   }
 }
