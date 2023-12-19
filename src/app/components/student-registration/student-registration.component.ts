8481fcd98dc91f347175f6ce54c7c1940001e402
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-19 09:42:55 +0530

    Create http post method to add students

--- src/app/components/student-registration/student-registration.component.ts
+++ src/app/components/student-registration/student-registration.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Student } from '../../Models/Student';
+import { NgToastModule, NgToastService } from 'ng-angular-popup';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-student-registration',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,NgToastModule],
   templateUrl: './student-registration.component.html',
   styleUrl: './student-registration.component.css',
 })
 export class StudentRegistrationComponent {
+  @ViewChild('form')
+  form!: NgForm;
+
   studentRegisterRequest: Student = {
-    studentId: ' ',
+    studentId: 0,
     firstname: ' ',
     lastname: ' ',
     birthday: new Date(),
@@ -24,7 +29,22 @@ export class StudentRegistrationComponent {
     addressline3: ' ',
   };
 
+  constructor(
+    private studentService: ApiService,
+    private toast: NgToastService){
+
+  }
+
   registerStudent() {
-    console.log(this.studentRegisterRequest);
+    this.studentService.registerStudent(this.studentRegisterRequest)
+    .subscribe({
+      next: (student) => {
+        this.form.resetForm();
+        this.toast.success({detail:"SUCCESS",summary:"Student Registered! ",duration:3000});
+      },
+      error: (response) => {
+
+      }
+    })
   }
 }
