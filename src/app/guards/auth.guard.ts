801102cbc5079886047654c7a995ff5b2936d27e
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-19 14:42:26 +0530

    Auth guard cleanup

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,11 +4,15 @@ import { inject } from '@angular/core';
 import { NgToastService } from 'ng-angular-popup';
 import { StateService } from '../services/state.service';
 
-export const authGuard: CanActivateFn = ():boolean => {
-  if(inject(StateService).loginStatus$){
+export const authGuard: CanActivateFn = (): boolean => {
+
+  if (inject(StateService).loginStatus$) {
     return true;
-  }else{
-    inject(NgToastService).error({detail:"ERROR",summary:"Please Login First"});
+  } else {
+    inject(NgToastService).error({
+      detail: 'ERROR',
+      summary: 'Please Login First',
+    });
     inject(Router).navigate(['/login']);
     return false;
   }
