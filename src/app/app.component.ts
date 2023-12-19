ee3f8e935d49050c151d2842dd0ce4f00ccd1436
Sakuniw <ayonaweerasinghe@gmail.com>
2023-12-19 08:52:27 +0530

    add angular popup when login form submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from "./components/core/header/header.component";
-import { FooterComponent } from "./components/core/footer/footer.component";
+import { NgToastModule } from 'ng-angular-popup';
+import { HeaderComponent } from './components/core/header/header.component';
+import { FooterComponent } from './components/core/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HttpClientModule,HeaderComponent, FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    NgToastModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'Student_Registration_System-Frontend';
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,11 +9,13 @@ import {
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { NgToastModule } from 'ng-angular-popup';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule,NgToastModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   providers: [AuthService],
@@ -24,7 +26,8 @@ export class LoginComponent {
   constructor(
     private fb: FormBuilder,
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private toast: NgToastService
   ) {}
 
   ngOnInit(): void {
@@ -41,12 +44,12 @@ export class LoginComponent {
       console.log(this.loginForm.value);
       this.auth.login(this.loginForm.value).subscribe({
         next: (res) => {
-          alert(res.message);
           this.loginForm.reset();
+          this.toast.success({detail:"SUCCESS",summary:res.message,duration:3000});
           this.router.navigate(['dashboard']);
         },
         error: (err) => {
-          alert(err?.error.message);
+          this.toast.error({detail:"ERROR",summary:"Something Went Wrong!",duration:3000});
         },
       });
     } else {
